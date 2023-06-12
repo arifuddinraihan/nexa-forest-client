bcc7664aec129591458b75a2865f1f10275fc28b
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-06-12 23:43:35 +0600

    added multiple section on homepage like location, team, pricing, blog, youtube contents, web stats, testimonials, etc. Need to edit the margin padding and dark mode with images and video contents

--- src/components/Pages/MainPage/Home/Home.js
+++ src/components/Pages/MainPage/Home/Home.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import useTitle from '../../../../hooks/useTitle';
-import Cover from '../Cover/Cover';
-import NewsLetter from '../NewsLetter/NewsLetter';
-import Services from '../Services/Services';
-import Subscription from '../Subscription/Subscription';
-import WorkGallery from '../WorkGallery/WorkGallery';
-import WebFeatures from '../WebFeatures/WebFeatures';
+import React from "react";
+import useTitle from "../../../../hooks/useTitle";
+import Cover from "../Cover/Cover";
+import NewsLetter from "../NewsLetter/NewsLetter";
+import Services from "../Services/Services";
+import Subscription from "../Subscription/Subscription";
+import WorkGallery from "../WorkGallery/WorkGallery";
+import WebFeatures from "../WebFeatures/WebFeatures";
+import HomeBlog from "../HomeBlog/HomeBlog";
+import HomeContents from "../HomeContents/HomeContents";
+import ShopLocation from "../ShopLocation/ShopLocation";
+import WebSubscription from "../WebSubscription/WebSubscription";
+import WebStats from "../WebStats/WebStats";
+import OurTeam from "../OurTeam/OurTeam";
+import ClientTestimonials from "../ClientTestimonials/ClientTestimonials";
 
 const Home = () => {
-    useTitle("NexaForest- Home")
-    return (
-        <div className='container mx-auto'>
-            <Cover></Cover>
-            <Services></Services>
-            <WebFeatures></WebFeatures>
-            <Subscription></Subscription>
-            <WorkGallery></WorkGallery>
-            <NewsLetter></NewsLetter>
-        </div>
-    );
+  useTitle("NexaForest- Home");
+  return (
+    <div className="container mx-auto">
+      <Cover></Cover>
+      <Services></Services>
+      <WebFeatures></WebFeatures>
+      <WebStats></WebStats>
+      <Subscription></Subscription>
+      <HomeBlog></HomeBlog>
+      <WorkGallery></WorkGallery>
+      <HomeContents></HomeContents>
+      <ClientTestimonials></ClientTestimonials>
+      <WebSubscription></WebSubscription>
+      <OurTeam></OurTeam>
+      <ShopLocation></ShopLocation>
+      <NewsLetter></NewsLetter>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Pages/MainPage/ShopLocation/ShopLocation.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const ShopLocation = () => {
+  const mapStyles = {
+    filter: 'grayscale(1) contrast(1.2)',
+    opacity: '0.4',
+  };
+  return (
+    <section className="text-gray-600 body-font relative">
+      <div className="absolute inset-0 bg-gray-300">
+        <iframe
+          width="100%"
+          height="100%"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          title="map"
+          scrolling="no"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14605.023571775708!2d90.4191933!3d23.7739004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c73b4f003d7b%3A0xd7df8bcdafb65203!2sNexa%20Limited!5e0!3m2!1sen!2sbd!4v1686590155871!5m2!1sen!2sbd"
+          style={mapStyles}
+          // style="filter: grayscale(1) contrast(1.2) opacity(0.4);"
+        ></iframe>
+      </div>
+      <div className="container px-5 py-24 mx-auto flex">
+        <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+            Feedback
+          </h2>
+          <p className="leading-relaxed mb-5 text-gray-600">
+            Post-ironic portland shabby chic echo park, banjo fashion axe
+          </p>
+          <div className="relative mb-4">
+            <label for="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label for="message" className="leading-7 text-sm text-gray-600">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          <button className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+            Button
+          </button>
+          <p className="text-xs text-gray-500 mt-3">
+            Chicharrones blog helvetica normcore iceland tousled brook viral
+            artisan.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ShopLocation;
