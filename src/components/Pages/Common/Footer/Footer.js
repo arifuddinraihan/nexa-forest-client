9aa250489e0fd024b2274be435e2fa1846a687a6
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-06-13 22:40:29 +0600

    added footer changes

--- src/components/Pages/Common/Footer/Footer.js
+++ src/components/Pages/Common/Footer/Footer.js
@@ -95,7 +95,7 @@ const Footer = () => {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <span className="text-sm text-gray-500 text-left dark:text-gray-400">
               © 2022<p className="hover:underline">NexaForest</p> All Rights
               Reserved
             </span>
